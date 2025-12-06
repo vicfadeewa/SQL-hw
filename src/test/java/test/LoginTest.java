ee42b9c25dbf78d1e9c14d9e379865738f05a8d2
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2025-12-06 19:53:52 +0300

    Добавлены docker-compose.yml, schema, тесты и обновлен build.gradle

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,71 @@
+package test;
+
+import org.junit.jupiter.api.*;
+import data.DataHelper;
+import data.SqlHelper;
+import pages.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.SqlHelper.cleanAuthCodes;
+import static data.SqlHelper.cleanDatabase;
+
+class LoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Позитивный тест")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SqlHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Неверно указано имя пользователя")
+    void shouldErrorInvalideLogin() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Неверно указан код верификации")
+    void shouldInvalidCodes() {
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан код! Попробуйте ещё раз.");
+    }
+
+    @Test
+    @DisplayName("Ввод неправильного пароля 3 раза")
+    void shouldLockAfterThreeUnsuccessfulPasswords() {
+        var authInfo = DataHelper.generateRandomUser();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+        for (int count = 0; count < 3; count++) {
+            loginPage.validLogin(DataHelper.generateRandomUser());
+        }
+        verificationPage.verifyErrorNotification("Превышено максимальное количество попыток авторизации");
+    }
+}
